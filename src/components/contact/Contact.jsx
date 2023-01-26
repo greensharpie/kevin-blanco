d38099b788de6a42c682717844f2e5b039cffbfc
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-26 16:37:39 -0500

    contact form linked to emailjs and receiving emails.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,16 +6,17 @@ import {BsWhatsapp} from 'react-icons/bs'
 import emailjs from 'emailjs-com'
 
 const Contact = () => {
-  const form = useRef;
+  const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_dljvvcj', 'template_lrvx64t', form.current, 'jSA1wpDMfVHw8rAJn')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+    e.target.reset()
   };
   return (
     <section id='contact'>
@@ -45,7 +46,7 @@ const Contact = () => {
         </div>
         <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Name'required/>
-          <input type='email' name='email' placeholder='Your E-mail'required/>
+          <input type='email' name='email' placeholder='Your Email'required/>
           <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
           <button type='submit' className='btn btn-primary'>Send</button>
         </form>
