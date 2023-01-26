1a3d7dec61f19dcca91e701481a0fdee9a3244a7
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-26 15:48:02 -0500

    added class name to icons. Form styling began

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,29 +13,29 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             {/* <h5>kevinjblanco@gmail.com</h5> */}
             <a href='mailto:kevinjblanco@gmail.com' target='_blank'>Send a message</a>
           </article>
           <article className='contact__option'>
-            <BsLinkedin/>
+            <BsLinkedin className='contact__option-icon'/>
             <h4>LinkedIn</h4>
             {/* <h5>Let's Connect</h5> */}
             <a href='https://www.linkedin.com/in/kevinjblanco/' target='_blank'>Let's Connect</a>
           </article>
           <article className='contact__option'>
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>Phone</h4>
             {/* <h5>+123456789</h5> */}
             <a href='' target='_blank'>Ring Me</a>
           </article>
         </div>
         <form action=''>
-          <input type='text' name='name' placeholder='Full Name'required/>
+          <input type='text' name='name' placeholder='Your Name'required/>
           <input type='email' name='email' placeholder='Your E-mail'required/>
           <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <button type='submit' className='btn btn-primary'>Send</button>
         </form>
       </div>
     </section>
