a4da3b556d8e184785433a1347909b63caaa059c
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-26 15:40:10 -0500

    contact form created but not styled

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            {/* <h5>kevinjblanco@gmail.com</h5> */}
+            <a href='mailto:kevinjblanco@gmail.com' target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsLinkedin/>
+            <h4>LinkedIn</h4>
+            {/* <h5>Let's Connect</h5> */}
+            <a href='https://www.linkedin.com/in/kevinjblanco/' target='_blank'>Let's Connect</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp/>
+            <h4>Phone</h4>
+            {/* <h5>+123456789</h5> */}
+            <a href='' target='_blank'>Ring Me</a>
+          </article>
+        </div>
+        <form action=''>
+          <input type='text' name='name' placeholder='Full Name'required/>
+          <input type='email' name='email' placeholder='Your E-mail'required/>
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,61 +1,39 @@
 import React from 'react'
 import './portfolio.css'
 
+const data = [
+  {
+    id: 1,
+    image: 'IMG1',
+    title: 'First Project Title',
+    github: 'link to github',
+    demo: 'link to live demo'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portofolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src='' alt='portfolio1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className='btn'target='_blank'>GitHub</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live</a>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src='' alt='portfolio1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className='btn'target='_blank'>GitHub</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live</a>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src='' alt='portfolio1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className='btn'target='_blank'>GitHub</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live</a>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src='' alt='portfolio1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className='btn'target='_blank'>GitHub</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live</a>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src='' alt='portfolio1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className='btn'target='_blank'>GitHub</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live</a>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src='' alt='portfolio1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className='btn'target='_blank'>GitHub</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live</a>
-        </article>
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+                  <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}/>
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn'target='_blank'>GitHub</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live</a>
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
