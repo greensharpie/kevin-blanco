4a5f00a722209a65396401e27c7b747f0744e244
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-26 13:40:35 -0500

    portfolio jsx skeleton began

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,61 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portofolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='' alt='portfolio1'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'target='_blank'>GitHub</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='' alt='portfolio1'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'target='_blank'>GitHub</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='' alt='portfolio1'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'target='_blank'>GitHub</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='' alt='portfolio1'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'target='_blank'>GitHub</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='' alt='portfolio1'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'target='_blank'>GitHub</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='' alt='portfolio1'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'target='_blank'>GitHub</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
