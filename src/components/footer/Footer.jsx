0f5b63fcf929dca546da21fcd7e0be9506dead04
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-27 09:41:35 -0500

    footer page icon imported

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#'className='footer__logo'>Kevin Blanco</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Experience</a></li>
+        <li><a href='#'>Portfolio</a></li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://www.linkedin.com/in/kevinjblanco/'><BsLinkedin/></a>
+        <a href='https://github.com/greensharpie'><BsGithub/></a>
+      </div>
+    </footer>
   )
 }
 
