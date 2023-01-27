2c2e23ad6e1fc963fa1bd94f5c627f40bea46461
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-27 14:59:25 -0500

    added project links to github and live links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,10 +5,45 @@ const data = [
   {
     id: 1,
     image: 'IMG1',
-    title: 'First Project Title',
-    github: 'link to github',
-    demo: 'link to live demo'
-  }
+    title: 'Tetris Replica',
+    github: 'https://github.com/greensharpie/Tetris',
+    demo: 'https://main.d28pdf8t2f2caq.amplifyapp.com/'
+  },
+  {
+    id: 2,
+    image: 'IMG2',
+    title: 'Door Drop',
+    github: 'https://github.com/greensharpie/Door_Drop_Frontend',
+    demo: 'https://main.djedseh58i37e.amplifyapp.com/'
+  },
+  {
+    id: 3,
+    image: 'IMG3',
+    title: 'ReVinyl',
+    github: 'https://github.com/mashbash2150/ReVinyl-Frontend',
+    demo: 'https://revinyl-frontend.herokuapp.com/'
+  },
+  {
+    id: 4,
+    image: 'IMG4',
+    title: 'Bookshelf',
+    github: 'https://github.com/greensharpie/BookShelf',
+    demo: 'https://bookshelf-crud.herokuapp.com/'
+  },
+  {
+    id: 5,
+    image: 'IMG5',
+    title: 'War - The Card Game',
+    github: 'https://github.com/greensharpie/kevins-war-cardgame',
+    demo: 'https://war-the-cardgame.surge.sh/'
+  },
+  {
+    id: 6,
+    image: 'IMG1',
+    title: 'Coasterpedia',
+    github: 'https://github.com/julialima08/Coasterpedia',
+    demo: 'https://coasterpedia-app.herokuapp.com/'
+  },
 ]
 
 const Portfolio = () => {
