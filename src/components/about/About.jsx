adb6c4bb68de34ff4d386b41ae70cdf6bb29a6eb
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-28 10:28:24 -0500

    added about me content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,7 +20,10 @@ const About = () => {
               <h5>Experience</h5>
             </article> 
           </div> */}
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque reprehenderit deserunt quam id quis ipsa, esse dolor facilis voluptatum, aliquam sit aut, animi consequatur molestiae? Maxime nulla distinctio natus architecto.</p>
+          <p> I discovered my passion for helping people through my work as an Athletic Trainer in the healthcare industry. I loved being able to make a positive impact on people's lives, but I knew I wanted to do more. That's when I decided to make a change and transition into the world of coding.</p>
+          <p>I spent months learning new skills and working on projects independently and with teams. I became proficient in languages like JavaScript, and React. I was able to apply the problem-solving skills that I gained as a healthcare professional from working in traditional settings and also from working for a global company like Boeing.
+Now, I am excited to take on a new challenge in the tech industry and I am eager to collaborate with like-minded individuals on projects.</p>
+<p>When I'm not coding, you can usually find me outside hiking/backpacking, trying a new restaurant, or going on walks with my dog and wife. I would love to hear from you, and I invite you to take a look at my portfolio to see how I could make a positive impact on your project. </p>
           <a href='#contact' className='btn btn-primary'>Let's Chat!</a>
         </div>
       </div>
