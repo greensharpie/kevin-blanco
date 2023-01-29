c5913611e3e073c23646296ec36c6f67849b4783
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-29 16:35:30 -0500

    possible resume pdf fix

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -4,7 +4,7 @@ import KEVINBLANCO_RESUME from '../../assets/KEVINBLANCO_RESUME.pdf'
 const CallToAction = () => {
   return (
     <div className='CallToAction'>
-      <a href={KEVINBLANCO_RESUME} download className='btn'>Download Resume</a>
+      <a href={KEVINBLANCO_RESUME} className='btn'>Download Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Chat</a>
     </div>
   )
