24b7269a8effe37c71a02d353300c95eefb1fbcf
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-29 23:22:16 -0500

    open link in new tab

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -5,7 +5,7 @@ const CallToAction = () => {
   return (
     <div className='CallToAction'>
       {/* <a href={Resume} download className='btn'>Download Resume</a> */}
-      <a href='https://docs.google.com/document/d/e/2PACX-1vSqJ-v7f3kM_yS0ol2FQJA6iJ2jY9cI6zIPEdhDI_Aj_EtF6Z7QtCmCMkJmd0xLx2XAKf2IoDhV_GTM/pub' download className='btn'>View Resume</a>
+      <a href='https://docs.google.com/document/d/e/2PACX-1vSqJ-v7f3kM_yS0ol2FQJA6iJ2jY9cI6zIPEdhDI_Aj_EtF6Z7QtCmCMkJmd0xLx2XAKf2IoDhV_GTM/pub' download className='btn' target='_blank'>View Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Chat</a>
     </div>
   )
