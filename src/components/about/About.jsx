901fb5493eb888937b61aa82f6b7b37d7ae90630
Kevin Blanco <kevinjblanco@gmail.com>
2025-02-15 15:19:59 -0500

    chore: update about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,15 +10,15 @@ const About = () => {
 
       <div className="container about__container">
         <div className='about__me'>
-        <div className="about__me-image">
-          <img src={PFP2} alt='about'/>
-        </div>
+          <div className="about__me-image">
+            <img src={PFP2} alt='about' />
+          </div>
         </div>
 
         <div className='about__content'>
-          <p> I discovered my passion for helping people through my work as an Athletic Trainer in the healthcare industry. I loved being able to make a positive impact on people's lives, but I knew I wanted to do more. That's when I decided to make a change and transition into the world of coding.</p>
-          <p>I spent months learning new skills and working on projects independently and with teams. I became proficient in languages like JavaScript, and React. I was able to apply the problem-solving skills that I gained as a healthcare professional from working in traditional settings and also from working for a global company like Boeing. Now, I am excited to take on a new challenge in the tech industry and I am eager to collaborate with like-minded individuals on projects.</p>
-          <p>When I'm not coding, you can usually find me outside hiking/backpacking, trying a new restaurant, or going on walks with my dog and wife. I would love to hear from you, and I invite you to take a look at my portfolio to see how I could make a positive impact on your project. </p>
+          <p> I discovered my passion for problem-solving and helping people through my work as an Athletic Trainer in the healthcare industry. While I loved making a direct impact on people's lives, I knew I wanted to do more—so I transitioned into the world of software engineering.</p>
+          <p>Over the past two years as a Software Engineer at Ally Financial, I've worked on web and mobile projects, collaborated across agile teams, and developed scalable, accessible solutions. My background in healthcare and experience working for a global company like Boeing have shaped my ability to adapt, think critically, and tackle complex challenges. I thrive in collaborative environments where I can leverage my skills in JavaScript, TypeScript, React.js, and React Native to build high-quality software.</p>
+          <p>When I'm not coding, you can usually find me hiking, backpacking, trying a new restaurant, or going on walks with my dog and wife. I'd love to connect—feel free to check out my portfolio and see how I can contribute to your next project. </p>
           <a href='#contact' className='btn btn-primary'>Let's Chat!</a>
         </div>
       </div>
