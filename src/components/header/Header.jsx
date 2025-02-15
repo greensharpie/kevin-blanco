9e7be9f1a214d7d449020e58bb3842e63615e868
Kevin Blanco <kevinjblanco@gmail.com>
2025-02-15 16:05:56 -0500

    chore: update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,11 +10,11 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Kevin Blanco</h1>
-        <h5>and I'm a Software Developer</h5>
+        <h5>and I'm a Software Engineer</h5>
         <CallToAction />
         <HeaderSocials />
         <div className='me'>
-          <img src={PFP} alt='me' className='pfp'/>
+          <img src={PFP} alt='me' className='pfp' />
         </div>
 
         <a href='#portfolio' className='scroll__down'>Portfolio</a>
