471cadd7b2e2a713c1faba1a7eee2fde91fc2279
Kevin Blanco <kevinjblanco@gmail.com>
2025-02-15 15:58:44 -0500

    chore: update resume

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 // import Resume from '../../assets/KEVINBLANCO_RESUME.pdf'
 
-export default function CallToAction () {
+export default function CallToAction() {
   return (
     <div className='CallToAction'>
-      <a href='https://drive.google.com/file/d/1QgMt5UWSGclridt_7_N00MI5UoWaixOv/view?usp=sharing' target='_blank' className='btn'>View Resume</a>
+      <a href='https://drive.google.com/file/d/1dirlaGhV6JW4YwZNMGWRlzPErx2MOiB-/view?usp=sharing' target='_blank' className='btn'>View Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Chat</a>
     </div>
   )
