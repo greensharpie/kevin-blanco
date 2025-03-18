55b5f8be70ace30321645e487ceb3e214ed51486
Kevin Blanco <kevinjblanco@gmail.com>
2025-03-18 16:24:05 -0400

    chore: update resume

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function CallToAction() {
   return (
     <div className='CallToAction'>
-      <a href='https://drive.google.com/file/d/1dirlaGhV6JW4YwZNMGWRlzPErx2MOiB-/view?usp=sharing' target='_blank' className='btn'>View Resume</a>
+      <a href='https://drive.google.com/file/d/1Y7Bua3x-tEAenMdqZESJbyPddmwT-d99/view?usp=sharing' target='_blank' className='btn'>View Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Chat</a>
     </div>
   )
